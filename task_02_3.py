e9b776b41cf70085646dd7695be51ab87cf8bad4
KseniaFinskaya <37531175+KseniaFinskaya@users.noreply.github.com>
2018-05-23 18:49:18 +0300

    corrected line lenght to task_02

--- task_02_3.py
+++ task_02_3.py
@@ -1,5 +1,5 @@
 """ Task "FizzBuzz": Write a program that prints the numbers from 1 to 100,
-but for multiples of three print “Fizz” instead of the number and for multiples of five print “Buzz”.
+but for multiples of three print “Fizz” instead of the number and for multiples of five print“Buzz”.
 For numbers which are multiples of both three and five, print “FizzBuzz”."""
 
 i = 0
