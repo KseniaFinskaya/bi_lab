b2724a5b80b7ffe59eb69f18840706cad00efedf
KseniaFinskaya <37531175+KseniaFinskaya@users.noreply.github.com>
2018-05-24 10:36:37 +0300

    Delete task_02_1.py

--- task_02_1.py
+++
@@ -1,6 +0,0 @@
-word = input('Enter word for palindrome check: ')
-revword = word[:: -1]
-if word == revword:
-    print('True, ', revword, '.', sep='')
-else:
-    print('False, ', revword, '.', sep='')
