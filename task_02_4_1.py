c3642c50ec852ec4659aca0641b9942c0644385d
KseniaFinskaya <37531175+KseniaFinskaya@users.noreply.github.com>
2018-05-24 10:28:16 +0300

    Delete task_02_4_1.py

--- task_02_4_1.py
+++
@@ -1,9 +0,0 @@
-import re
-password = str(input('Enter password: '))
-rules = [re.match("[a-zA-Z0-9]+", password),
-         10 <= len(password),
-         ]
-if all(rules):
-    print('True')
-else:
-    print('False')
