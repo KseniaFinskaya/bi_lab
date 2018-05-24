1faa935d7ca027f39e9c416034103c4fd6c63d13
KseniaFinskaya <37531175+KseniaFinskaya@users.noreply.github.com>
2018-05-24 10:33:04 +0300

    new lines at the end of scripts added

--- task_2_2.py
+++ task_2_2.py
@@ -1,4 +1,4 @@
 item_price = input('Enter item price, for example, 3.20: ')
 items_brought = input('Enter items brought, for example, 5: ')
-ttl_cost = float(item_price)*int(items_brought)
+ttl_cost = float(item_price) * int(items_brought)
 print('Total price is ', ttl_cost, '.', sep='')
