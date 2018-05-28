fabb33177855f381a0707e47ec067c3bf779d77b
KseniaFinskaya <37531175+KseniaFinskaya@users.noreply.github.com>
2018-05-28 10:23:56 +0300

    scr task_2_2 updated

--- task_2_2.py
+++ task_2_2.py
@@ -1,5 +1,5 @@
 item_dollars = int(input('Enter item price - dollars, for example, 3: '))
 item_cents = int(input('Enter item price - cents, for example, 20: '))
 items_brought = int(input('Enter items brought, for example, 5: '))
-ttl_cost = (item_dollars + item_cents/100) * items_brought
+ttl_cost = (item_dollars + item_cents / 100) * items_brought
 print('Total price is ', ttl_cost, '.', sep='')
