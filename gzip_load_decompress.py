3c10024e8846a6b0b1de282dd71c84f33a25a8d7
KseniaFinskaya <Ksenia_Finskaya@epam.com>
2018-06-03 09:22:40 +0300

    decompress task5 corrected

--- gzip_load_decompress.py
+++ gzip_load_decompress.py
@@ -1,16 +1,27 @@
 from ftplib import FTP
 import gzip
+import os.path
 import shutil
+import re
 
 
-with FTP() as ftp:
-    ftp.set_pasv(True)
-    ftp.connect('ftp.fu-berlin.de', timeout=120)
-    ftp.login()
-    ftp.cwd('/pub/misc/movies/database/frozendata/')
-    filename = 'ratings.list.gz'
-    with open(filename, 'wb') as lf:
-        ftp.retrbinary('RETR ' + filename, lf.write)
-        with gzip.open(filename, 'rb') as gfile:
-            with open('ratings.list', 'wb') as f_out:
-                shutil.copyfileobj(gfile, f_out)
+url = 'ftp.fu-berlin.de/pub/misc/movies/database/frozendata/ratings.list.gz'
+site = url.split('/')[0]
+print(url)
+localfile = url.split('/')[-1]
+filename = re.match(r'^([\w]+.[\w]+)', localfile).group()
+subdir = re.findall(r'/\S+/', url)[0]
+if os.path.exists(localfile):
+    print('File is already downloaded.')
+else:
+    with FTP() as ftp:
+        ftp.set_pasv(True)
+        ftp.connect(site, timeout=120)
+        ftp.login()
+        ftp.cwd(subdir)
+        with open(localfile, 'wb') as lf:
+            ftp.retrbinary('RETR ' + localfile, lf.write)
+            lf.close()
+            with gzip.open(localfile, 'rb') as gfile:
+                with open(filename, 'wb') as f_out:
+                    shutil.copyfileobj(gfile, f_out)
